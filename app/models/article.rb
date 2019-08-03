74c6750e29c57bae1921793200c5d0c69a336ddb
Irvin Reyes <reyes.yerp@gmail.com>
2019-08-03 11:32:26 -0500

    Add the annotate gem

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :integer          not null, primary key
+#  text       :text
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Article < ApplicationRecord
   validates :title, presence: true, length: { minimum: 5 }
 end
