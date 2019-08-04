d44871f2c2e32e43538d5993e7484d8e30728043
Irvin Reyes <reyes.yerp@gmail.com>
2019-08-04 12:31:34 -0500

    Add basic dummy authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "iv", password: "123", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
